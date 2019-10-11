56cc7d537af3fd04a29917c87e9b9a140456f40b
Krishan Nattar <krishan.nattar@gmail.com>
2019-10-11 09:00:04 -0700

    adding generic user and role services

---
+++ javatodo/src/main/java/com/krishannattar/sprint/repos/RoleRepository.java
@@ -0,0 +1,8 @@
+package com.krishannattar.sprint.repos;
+
+import com.krishannattar.sprint.models.Role;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RoleRepository extends CrudRepository<Role, Long> {
+
+}
