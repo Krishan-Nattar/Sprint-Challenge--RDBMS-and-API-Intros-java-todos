9d69a72ecfa929e150d53353ac632b5f52551d2d
Krishan Nattar <krishan.nattar@gmail.com>
2019-10-11 09:28:49 -0700

    Testing getUsername

--- javatodo/src/main/java/com/krishannattar/sprint/controllers/UserController.java
+++ javatodo/src/main/java/com/krishannattar/sprint/controllers/UserController.java
@@ -1,4 +1,21 @@
 package com.krishannattar.sprint.controllers;
 
+import com.krishannattar.sprint.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping(value = "/users/mine", produces = {"application/json"})
+    public ResponseEntity<?> getUsername(Authentication authentication)
+    {
+        return new ResponseEntity<>(userService.findUserByName(authentication.getName()), HttpStatus.OK);
+    }
 }
